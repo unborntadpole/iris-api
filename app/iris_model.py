27ea5716c91804187b920947e84e23b4479487b9
unborntadpole <unborntadpole@gmail.com>
2025-07-26 09:32:19 +0530

    directory error

--- app/iris_model.py
+++ app/iris_model.py
@@ -1,11 +1,15 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 import pickle
+import os
 
 iris = load_iris()
 X, y = iris.data, iris.target
 model = LogisticRegression(max_iter=200)
 model.fit(X, y)
+
+os.makedirs("model", exist_ok=True)
+
 
 with open("model/model.pkl", "wb") as f:
     pickle.dump(model, f)
