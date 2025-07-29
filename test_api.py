7b6619acab44daaf69d72c0202d251f12111d23b
Samriddh Singh <samriddhsingh@Samriddhs-MacBook-Air.local>
2025-07-30 02:30:30 +0530

    Hello

--- test_api.py
+++ test_api.py
@@ -2,8 +2,6 @@ from fastapi.testclient import TestClient
 from main import app
 
 # import sys
-# import os
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 client = TestClient(app)
 
